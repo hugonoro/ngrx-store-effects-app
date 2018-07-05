0c62d315e7ba7a4016293ecf6e038628f4ad5205
Hugo Noro <norogithub@gmail.com>
2018-07-05 14:38:33 +0100

    State composition with selectors

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,40 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
+import * as fromStore from '../../store';
 
 @Component({
-  selector: 'products',
-  styleUrls: ['products.component.scss'],
-  template: `
-    <div class="products">
-      <div class="products__new">
-        <a
-          class="btn btn__ok" 
-          routerLink="./new">
-          New Pizza
-        </a>
-      </div>
-      <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
-          No pizzas, add one to get started.
+    selector: 'products',
+    styleUrls: ['products.component.scss'],
+    template: `
+      <div class="products">
+        <div class="products__new">
+          <a
+                  class="btn btn__ok"
+                  routerLink="./new">
+            New Pizza
+          </a>
         </div>
-        <pizza-item
-          *ngFor="let pizza of (pizzas)"
-          [pizza]="pizza">
-        </pizza-item>
+        <div class="products__list">
+          <div *ngIf="!((pizzas$ | async)?.length)">
+            No pizzas, add one to get started.
+          </div>
+          <pizza-item
+                  *ngFor="let pizza of (pizzas$ | async)"
+                  [pizza]="pizza">
+          </pizza-item>
+        </div>
       </div>
-    </div>
-  `,
+    `,
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+    pizzas$: Observable<Pizza[]>;
 
-  constructor(private pizzaService: PizzasService) {}
+    constructor(private store: Store<fromStore.ProductState>) {
+    }
 
-  ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
-    });
-  }
+    ngOnInit() {
+        this.pizzas$ = this.store.select(fromStore.getAllPizzas);
+    }
 }
--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,6 +1,6 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 
-import * as fromPizzas from './pizzas.reducer'
+import * as fromPizzas from './pizzas.reducer';
 
 export interface ProductState {
     pizzas: fromPizzas.PizzaState
@@ -8,4 +8,12 @@ export interface ProductState {
 
 export const reducers: ActionReducerMap<ProductState> = {
     pizzas: fromPizzas.reducer
-}
+};
+
+export const getProductsState = createFeatureSelector<ProductState>('products');
+
+export const getPizzaState = createSelector(getProductsState, (state: ProductState) => state.pizzas);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
