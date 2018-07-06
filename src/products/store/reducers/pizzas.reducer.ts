1180f1aa83fe1c582039492aac606594b9ea0036
Hugo Noro <norogithub@gmail.com>
2018-07-06 14:57:56 +0100

    First Effect

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,4 +1,4 @@
-import * as fromPizzas from '../actions/pizzas.action'
+import * as fromPizzas from '../actions/pizzas.action';
 import { Pizza } from '../../models/pizza.model';
 
 export interface PizzaState {
@@ -9,48 +9,7 @@ export interface PizzaState {
 
 
 export const initialState: PizzaState = {
-    data: [{
-        "name": "Seaside Surfin'",
-        "toppings": [
-            {
-                "id": 6,
-                "name": "mushroom"
-            },
-            {
-                "id": 7,
-                "name": "olive"
-            },
-            {
-                "id": 2,
-                "name": "bacon"
-            },
-            {
-                "id": 3,
-                "name": "basil"
-            },
-            {
-                "id": 1,
-                "name": "anchovy"
-            },
-            {
-                "id": 8,
-                "name": "onion"
-            },
-            {
-                "id": 11,
-                "name": "sweetcorn"
-            },
-            {
-                "id": 9,
-                "name": "pepper"
-            },
-            {
-                "id": 5,
-                "name": "mozzarella"
-            }
-        ],
-        "id": 2
-    }],
+    data: [],
     loaded: false,
     loading: false,
 };
@@ -65,10 +24,12 @@ export function reducer(state = initialState, action: fromPizzas.PizzasAction): 
             };
         }
         case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+            const data = action.payload;
             return {
                 ...state,
                 loading: false,
-                loaded: true
+                loaded: true,
+                data
             };
         }
         case fromPizzas.LOAD_PIZZAS_FAIL: {
